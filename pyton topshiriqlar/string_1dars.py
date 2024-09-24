cca43bb57a06c65ca6fb402575d3123f6b7219a1
Abdullaev Bekzod <bekzodabdullaev727@gmail.com>
2024-09-24 00:35:18 -0700

    Yangi fayl qoshildi
    mening topshiriqlarim yuklaandi

---
+++ pyton topshiriqlar/string_1dars.py
@@ -0,0 +1,50 @@
+# name = "Abdullaev"
+# print(name +' ' "Bekzod")
+# first_name ="Bekzod"
+# string = "Mening ismim "
+# print(first_name + " " +string)
+
+# name = "James"
+# firstname = 'bond'
+# print(f"Mening ismim {name}. {name} {firstname}")
+
+# print("Assallomu \tallaykum")
+# print("Assallomu \nallaykum")
+# kod ishlamadi tushunmadim
+# world = 'Hello world!'
+# exsel = 'Hello !'
+# world_exsel = f"{world} {exsel}"
+# print(world_exsel.upper())
+
+
+# ism = "bekzod"
+# (ism.lower())
+
+
+# ism_sharif = "abdullev bekzod"
+# print(ism_sharif .title())
+
+# ism_sharif = "abdullev bekzod"
+# print(ism_sharif.capitalize())
+
+# name = input("Ismingiz nima?")
+# print("Assalomu alaykum, " +name)
+
+# kocha = "Bogbon"
+# mahalla = "Sogbon"
+# tuman = "Bodomzor"
+# viloyat = "samarqand"
+# print(f"{kocha} kochasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} Samarqand",)
+
+# kocha = input("Qaysi kochada yashaysiz? ")
+# mahalla = input("Mahalangizni nomi? ")
+# tuman = input("Tumaningizni kiriting? ")
+# viloyat = input("Viloyatingizni kiriting? ")
+# print("kocha")
+
+
+# ism = "Bekzod"
+# print(type(ism))
+
+def my_function():
+    
